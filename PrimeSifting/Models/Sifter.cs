d5d2de7ae0766979f6cb250f3ee728aaa55f7df4
me <me@me.me.com>
2020-07-31 15:55:21 -0700

    add specs for constructor

--- PrimeSifting/Models/Sifter.cs
+++ PrimeSifting/Models/Sifter.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace PrimeSifting.Models
 {
     public class Sifter
     {
-        
+        public List<string> _initalList;
+        public Sifter(int number){
+           string[] str = Enumerable.Repeat("O", number).ToArray();
+           str[0] = "X";
+           _initalList = new List<string>(str);
+        }
     }
 }
--- PrimeSiftingTests/ModelsTests/SiftingTests.cs
+++ PrimeSiftingTests/ModelsTests/SiftingTests.cs
@@ -1,7 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeSifting.Models;
+using System.Collections.Generic;
+
 namespace PrimeSiftingTests
 {
+    [TestClass]
     public class SiftingTests
     {
-        
+        [TestMethod]
+        public void Sifter_SifterConstructor_True(){
+            Sifter s = new Sifter(5);
+            Assert.AreEqual(typeof(Sifter),s.GetType());
+        }
+
+        [TestMethod]
+        public void Sifter_SifterConstructorCreatesList_True(){
+            //Arrange
+            Sifter s = new Sifter(5);
+            List<string> al = new List<string>{"X","O","O","O","O"};
+
+            //act
+            List<string> actl = s._initalList;
+            
+            CollectionAssert.AreEqual(al,actl);
+        }
+
+          [TestMethod]
+        public void Sifter_SifterConstructorCreatesList_False(){
+            //Arrange
+            Sifter s = new Sifter(5);
+            List<string> al = new List<string>{"X","X","O","O","O"};
+
+            //act
+            List<string> actl = s._initalList;
+            
+            CollectionAssert.AreNotEqual(al,actl);
+        }
     }
 }
